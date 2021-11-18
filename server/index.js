8687de2a74735181ae1030f649c20c8ad18e97d3
Santiago Rubio <s.rubio91@gmail.com>
2021-11-18 00:16:28 -0300

    limit check

--- server/index.js
+++ server/index.js
@@ -8,8 +8,7 @@ import imageRoutes from './routes/images.js';
 
 const app = express();
 dotenv.config();
-app.use(express.json());
-app.use(express.limit('50MB'));
+app.use(express.json({ limit: '150mb' }));
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
