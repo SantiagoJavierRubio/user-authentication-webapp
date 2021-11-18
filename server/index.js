4b67ef3f07a4e1e90bf578c9d1cfacac761a64b6
Santiago Rubio <s.rubio91@gmail.com>
2021-11-18 00:44:31 -0300

    limit check

--- server/index.js
+++ server/index.js
@@ -7,10 +7,9 @@ import authRoutes from './routes/auth.js';
 import imageRoutes from './routes/images.js';
 
 const app = express();
-app.use(express.bodyParser({ limit: '50mb' }));
 dotenv.config();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb'}));
 
 const PORT = process.env.PORT || 5000;
 
